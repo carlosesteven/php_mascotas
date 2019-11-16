3b68f6a3f887c5f46d1178f9f14b827d9b8464ff
carlosesteven <esteven96@gmail.com>
2019-11-16 15:01:42 -0500

    first commit

--- agregar_juego.php
+++ agregar_juego.php
@@ -5,9 +5,15 @@
 
     if( $_POST 
         && $_POST["juego_materiales"]
-        && $_POST["juego_foto"]        
+        && $_FILES        
     )
     {
+
+        $file_name = $_FILES['juego_foto']['name'];
+        $file_path = $_FILES['juego_foto']['tmp_name'];
+        
+        $extencion = explode(".", $file_name)[ 1 ];
+        $nombre_temporal = uniqid() . "." . $extencion;  
 
         $cliente = new MongoDB\Client("mongodb+srv://csc:8426803156229492@cluster0-5fixw.gcp.mongodb.net/test?retryWrites=true&w=majority");
 
@@ -16,11 +22,18 @@
         $insertOneResult = $collection->insertOne([
             'juego_id' => ( count( $collection->find()->toArray() ) + 1 ),
             'juego_materiales' => $_POST["juego_materiales"],
-            'juego_foto' => $_POST["juego_foto"],
+            'juego_foto' => $file_name,
         ]);
 
-        $respuesta["estado"] = "ok";
-        $respuesta["detalles"] = "Se registro el juego correctamente.";
+        if(move_uploaded_file($file_path, "planos/" . $nombre_temporal ))
+        {
+            $respuesta["estado"] = "ok";
+            $respuesta["detalles"] = "Se registro el juego correctamente.";
+        }else{
+            $respuesta["estado"] = "error";
+            $respuesta["detalles"] = "Error al guardar la imagen.";
+        }
+        
 
     }else{
         $respuesta["estado"] = "error";
